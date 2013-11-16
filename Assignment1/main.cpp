2051d133df292aae9b0964fb7731468b3a192244
Md. Abdul Munim Dibosh <sankalpo@MacAir.local>
2013-11-16 21:01:56 +0600

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Md. Abdul Munim Dibosh on 11/16/13.
+//  Copyright (c) 2013 Md. Abdul Munim Dibosh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
