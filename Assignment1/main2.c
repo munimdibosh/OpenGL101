ec8d7b52ea97691f5117617043ea99a84292e23a
Md. Abdul Munim Dibosh <sankalpo@MacAir.local>
2014-04-17 15:51:40 +0600

    Removed Unnecessary Files

--- Assignment1/main2.c
+++
@@ -1,9 +0,0 @@
-//
-//  main2.c
-//  Assignment1
-//
-//  Created by Md. Abdul Munim Dibosh on 1/16/14.
-//  Copyright (c) 2014 Md. Abdul Munim Dibosh. All rights reserved.
-//
-
-#include <stdio.h>
--- Assignment1/test.c
+++
@@ -1,9 +0,0 @@
-//
-//  test.c
-//  Assignment1
-//
-//  Created by Md. Abdul Munim Dibosh on 12/7/13.
-//  Copyright (c) 2013 Md. Abdul Munim Dibosh. All rights reserved.
-//
-
-#include <stdio.h>
